9b968f8181ec4724f124e1da77d59d0bc98a3d84
Nicolas Falciglio <falciglionicolas@gmail.com>
2023-11-16 14:22:50 -0300

    segunda-preentrega-react

--- moniessen/src/components/CartWidget/CartWidget.jsx
+++ moniessen/src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ import {button,img} from '../NavBar/NavBar.module.css'
     const itemCount = 5; 
     return (
       <div className="cart-widget">
-        <button className={button} ><img src="./src/assets/img/carrito.png" alt="" className={img} /></button>
+        <button className={button} ><img src="../../src/assets/img/carrito.png" alt="" className={img} /></button>
         <span className="badge bg-primary">{itemCount}</span>
       </div>
     );
--- moniessen/src/components/Logo/ImagenLogo.jsx
+++ moniessen/src/components/Logo/ImagenLogo.jsx
@@ -2,7 +2,7 @@ import {moniessen} from '../NavBar/NavBar.module.css'
 const Logo = () => {
     return (
         <div>
-        <a href="#"><img src="../assets/img/moniessen.png" alt="MoniEssen" className={moniessen}/></a>
+        <a href="#"><img src="../src/assets/img/moniessen.png" alt="MoniEssen" className={moniessen}/></a>
     </div>
     );
   };
